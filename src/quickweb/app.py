4c2d507e4a256d63450bae6ba9c6270e24a357be
Andrew Downing <andrew.c.downing@gmail.com>
2023-11-10 19:30:40 -0700

    first commit

---
+++ src/quickweb/app.py
@@ -0,0 +1,23 @@
+
+from .api import API
+
+"""
+This is the main entry point for the application."""
+app = API()
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from the HOME page"
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from the ABOUT page"
+
+@app.route("/hello/{name}")
+def greeting(request, response, name):
+    response.text = f"Hello, {name}"
+
+@app.route("/sum/{num_1:d}/{num_2:d}")
+def sum(request, response, num_1, num_2):
+    total = int(num_1) + int(num_2)
+    response.text = f"{num_1} + {num_2} = {total}"
